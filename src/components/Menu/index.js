06071a3057534c96bdbf4f03231dc889c4e4595d
Renan Porto <renan.porto1099@gmail.com>
2019-05-02 22:10:50 -0300

    Adicionando Menu

---
+++ src/components/Menu/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import QRCode from 'react-native-qrcode';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {
+  Container, Code, Nav, NavItem, NavText, SignOutButton, SignOutButtonText,
+} from './styles';
+
+export default function Menu() {
+  return (
+    <Container>
+      <Code>
+        <QRCode
+          value="https://github.com/tota1099"
+          size={80}
+          bgColor="#8B10AE"
+          fgColor="#FFF"
+        />
+      </Code>
+
+      <Nav>
+        <NavItem>
+          <Icon name="help-outline" size={20} color="#FFF" />
+          <NavText>Me ajuda</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="person-outline" size={20} color="#FFF" />
+          <NavText>Perfil</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="credit-card" size={20} color="#FFF" />
+          <NavText>Configurar cartão</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="smartphone" size={20} color="#FFF" />
+          <NavText>Configurações do app</NavText>
+        </NavItem>
+      </Nav>
+
+      <SignOutButton onPress={() => {}}>
+        <SignOutButtonText>
+          SAIR DO APP
+        </SignOutButtonText>
+      </SignOutButton>
+    </Container>
+  );
+}
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,40 @@
 import React from 'react';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
-import { Container } from './styles';
+import Menu from '~/components/Menu';
+
+import {
+  Container, Content, Card, CardHeader, CardContent, CardFooter, Annotation, Title, Description,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Menu />
+
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 197.611,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 20,00 recebida de Luiz Henrique hoje ás 6:03h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
